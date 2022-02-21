92ab36705e9ac1e32f9e44d1a47f23813724cbb6
lxifra <l.xifra.2021@alumnos.urjc.es>
2022-02-21 17:07:43 +0100

    Creation of calculate_weight

--- P1/Ex1 (NO HECHO).py
+++
@@ -1,5 +0,0 @@
-from Seq1 import Seq
-sequence = "ACTGA"
-print_seq = Seq.__str__(sequence)
-lenght = Seq.len(sequence)
-print("Sequence 1: (Length:", lenght, ")", print_seq)
---
+++ P1/Ex1.py
@@ -0,0 +1,5 @@
+from Seq1 import Seq
+sequence = Seq("ACTGA") #this are empy objects of the class
+print_seq = Seq.__str__(sequence)
+lenght = Seq.len(sequence)
+print(f"Sequence 1: (Lenght:", lenght, ")", print_seq)

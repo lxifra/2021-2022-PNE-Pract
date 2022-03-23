be9e1100aec416b513270bdae237217cf4dba4c0
lxifra <l.xifra.2021@alumnos.urjc.es>
2022-03-23 15:56:47 +0100

    Creation of calculate_weight

--- P2/Ex7.py
+++ P2/Ex7.py
@@ -3,14 +3,10 @@ from Client0 import Client
 PRACTICE = 2
 EXERCISE = 7
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
-#IP = "localhost"
-#PORT = 8080
-#c = Client(IP, PORT)
+
 seq = open("./sequences/" + "FRAT1.txt", "r").read()
 seq = seq[seq.find("\n"):].replace("\n", "")
 print("Gene FRAT:", seq)
-
-###msg = c.talk("Sending FRAT1 Gene to the server, in fragment of 10 bases... ")
 
 frag = ""
 seqnum = 0

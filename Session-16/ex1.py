c86e123e114416b6ecdf18befccb3454e84dbbd2
lxifra <l.xifra.2021@alumnos.urjc.es>
2022-04-19 16:41:39 +0200

    Creation of calculate_weight

---
+++ Session-16/ex1.py
@@ -0,0 +1,69 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+PORT = 8080
+
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        termcolor.cprint(self.requestline, 'green')
+        print(self.path)
+
+        if self.path == "/":
+            contents = Path("/form-1.html").read_text()
+        elif self.path.startswith("/echo?msg"):
+            message = self.path.split("?msg=")[1]
+            print("message is: ", message)
+            print(Path("/template.html").read_text())
+            contents = Path("/template.html").read_text().format(message)
+        else:
+            contents = Path("/Error.html").read_text()
+
+
+
+        # Open the form1.html file
+        # Read the index from the file
+        #contents = Path('form-1.html').read_text()
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()

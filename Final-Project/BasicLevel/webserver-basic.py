54783a9b59b9dffae75a08400c9826218e579a8b
lxifra <l.xifra.2021@alumnos.urjc.es>
2022-05-23 10:45:44 +0200

    Creation of calculate_weight

--- Final-Project/BasicLevel/webserver-basic.py
+++ Final-Project/BasicLevel/webserver-basic.py
@@ -16,105 +16,108 @@ def read_html_file(filename):
     contents = j.Template(contents)
     return contents
 
+def request_json(endpoint):
+    SERVER = 'rest.ensembl.org'
+    PARAMS = "?content-type=application/json"
+
+    conn = http.client.HTTPConnection(SERVER)
+    try:
+        conn.request("GET", endpoint + PARAMS)
+        r1 = conn.getresponse()
+        print(f"Response received!: {r1.status} {r1.reason}\n")
+
+        info_dict = r1.read().decode("utf-8")
+        info_dict = json.loads(info_dict)
+        return info_dict
+
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+
+
+
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
 
         termcolor.cprint(self.requestline, 'green')
-        print("PATH:---- >", self.path)
         url = urlparse(self.path)
-        termcolor.cprint(url, 'blue')
         arguments = parse_qs(url.query)
-        termcolor.cprint(arguments, 'yellow')
 
-        SERVER = 'rest.ensembl.org'
-        PARAMS = "?content-type=application/json"
 
         if self.path == "/":
             contents = read_html_file("index-basic.html").render(context={})
 
+
         elif self.path.startswith("/listSpecies?"):
             ENDPOINT = "info/species"
 
-            conn = http.client.HTTPConnection(SERVER)
+            classSpecies = request_json(ENDPOINT)
+            info_species = classSpecies["species"]
+            list_names = []
 
+            for c in info_species:
+                list_names.append(c["display_name"])
             try:
-                conn.request("GET", ENDPOINT + PARAMS)
-                r1 = conn.getresponse()
-                print(f"Response received!: {r1.status} {r1.reason}\n")
-
-                classSpecies = r1.read().decode("utf-8")
-                classSpecies = json.loads(classSpecies)
-                species = classSpecies["species"]
-                list_names = []
                 limit = arguments["limit"][0]
-                for c in species:
-                    list_names.append(c["display_name"])
-                    wanted_names = list_names[0:int(limit)]
-                contents = read_html_file("listSpecies.html").render(context={"limit": limit,
+                wanted_names = list_names[0:int(limit)]
+            except KeyError:
+                limit = len(list_names)
+                wanted_names = list_names[0:int(limit)]
+
+            contents = read_html_file("listSpecies.html").render(context={"limit": limit,
                                                                               "total_lenght": len(list_names),
                                                                               "wanted_list": wanted_names})
-            except ConnectionRefusedError:
-                print("ERROR! Cannot connect to the Server")
-                exit()
-
 
         elif self.path.startswith("/karyotype?"):
-            contents = "WE HAVE ENTERED THE KARYOTYPE"
             ENDPOINT = "info/assembly/"
             specie = arguments["specie"][0]
-
-            conn = http.client.HTTPConnection(SERVER)
-
+            classSpecies = request_json(ENDPOINT + specie)
             try:
-                conn.request("GET", ENDPOINT + specie + PARAMS)
-                r1 = conn.getresponse()
-                print(f"Response received!: {r1.status} {r1.reason}\n")
-
-                classSpecies = r1.read().decode("utf-8")
-                classSpecies = json.loads(classSpecies)
                 karyotype = classSpecies["karyotype"]
                 contents = read_html_file("karyotype.html").render(context={"karyotype": karyotype})
-            except ConnectionRefusedError:
-                print("ERROR! Cannot connect to the Server")
-                exit()
+            except KeyError:
+                contents = Path("error.html").read_text()
 
 
+        elif self.path.startswith("/lenght?"):
+            ENDPOINT = "info/assembly/"
 
+            specie = arguments["species"][0]
+            chromosome = arguments["chromosome"][0]
+            classSpecies = request_json(ENDPOINT + specie)
+            try:
+                if int(chromosome) >= len(classSpecies["top_level_region"]):
+                    contents = "This number is too big."
+                else:
+                    chromosome_dict = classSpecies["top_level_region"][int(chromosome)]
+                    lenght = chromosome_dict["length"]
+                    contents = read_html_file("chromosomelenght.html").render(context={"c_lenght": lenght})
+            except KeyError:
+                contents = Path("error.html").read_text()
 
         else:
             contents = Path("error.html").read_text()
 
 
 
-        self.send_response(200)  # -- Status line: OK!
+        self.send_response(200)
 
-        # Define the content-type header:
         self.send_header('Content-Type', 'text/html')
         self.send_header('Content-Length', len(str.encode(contents)))
 
-        # The header is finished
         self.end_headers()
-
-        # Send the response message
         self.wfile.write(str.encode(contents))
 
         return
 
 
-# ------------------------
-# - Server MAIN program
-# ------------------------
-# -- Set the new handler
 Handler = TestHandler
 
-# -- Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
     print("Serving at PORT", PORT)
 
-    # -- Main loop: Attend the client. Whenever there is a new
-    # -- clint, the handler is called
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
